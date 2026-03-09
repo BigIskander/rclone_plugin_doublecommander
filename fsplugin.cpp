a6aa67dfe4d6d3b60d3fa65464b1840b83f8e09d
BigIskander <bigiskander@gmail.com>
2026-03-09 20:27:04 +0300

    Browse files in cloud storage success.

--- fsplugin.cpp
+++ fsplugin.cpp
@@ -1,7 +1,35 @@
+/*
+Rclone plugin for Double Commander
+
+Wfx plugin for working with different cloud storages using Rclone.
+
+Copyright (C) 2026 Iskander Sultanov (BigIskander@gmail.com)
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+        License as published by the Free Software Foundation; either
+        version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+        Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+        Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
 #include <stdio.h>
 #include <cstring>
+#include <string>
+#include <array>
+#include <algorithm>
+#include <memory>
 #include "common.h"
 #include "fsplugin.h"
+#include "utils.hpp"
+#include "json.hpp"
 
 #define _plugin_name "Rclone"
 
@@ -21,21 +49,178 @@ int DCPCALL FsInitW(int PluginNr, tProgressProcW pProgressProc, tLogProcW pLogPr
     return 0;
 }
 
+bool isInit = false;
+bool isPut = false;
+
 HANDLE DCPCALL FsFindFirstW(WCHAR* Path, WIN32_FIND_DATAW *FindData)
 {
-    /* not implemented yet */
-    return (HANDLE)-1;
+    pResources pRes = NULL;
+
+    wcharstring wPath(Path);
+    if(wPath.length() == 0) return (HANDLE)-1;
+    std::replace(wPath.begin(), wPath.end(), u'\\', u'/');
+
+    // fix issue with traversing files and folders
+    if(wPath.size() > 1 && wPath.substr(wPath.size() - 1) == (WCHAR*)u"/")
+    {
+        wPath = wPath.substr(0, wPath.size() - 1);
+        // ignore this kind of request when put files, for speed
+        if(isPut) return (HANDLE)-1;
+    } 
+
+    // varianles used for request to rclone
+    std::array<char, 128> buffer;
+    std::vector<wcharstring> resultVector;
+    std::string resultString;
+
+    if(wPath.length() == 1) { // root folder of plugin
+        // gLogProc(gPluginNumber, MSGTYPE_CONNECT, (WCHAR*)u"123");
+        // request list of configured storages (available romotes) from rclone's config 
+        const char* command = "rclone listremotes";
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command, "r"), pclose);
+        
+        if (!pipe) return (HANDLE)-1; // popen failed
+
+        // Read the output line by line into the buffer
+        wcharstring resultLine;
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+            wcharstring itemName = UTF8toUTF16(buffer.data());
+            if(std::isspace(itemName.at(itemName.size() - 1))) // delete last element if it is space
+                itemName = itemName.substr(0, itemName.size() - 1);
+            resultVector.push_back(itemName);
+        }
+
+        // close popen
+        int status = pclose(pipe.release());
+        if(status != 0) {
+            // if error occured
+            gLogProc(gPluginNumber, MSGTYPE_IMPORTANTERROR, 
+                wcharstring((WCHAR*)u"Command (rclone listremotes) exited with status ").append(int_to_wcharstring(status)).data()
+            );
+            return (HANDLE)-1;
+        }
+
+        pRes = new tResources;
+        pRes->nCount = 0;
+        pRes->resource_array.resize(resultVector.size());
+
+        size_t str_size;
+        wcharstring itemName;
+        for(int i = 0; i < resultVector.size(); i++) 
+        {
+            itemName = resultVector[i].substr(resultVector.size() - 1); // delete last empty value
+            str_size = (MAX_PATH > resultVector[i].size()+1) ? 
+                (resultVector[i].size()+1): MAX_PATH;
+            memcpy(
+                pRes->resource_array[i].cFileName, 
+                resultVector[i].data(), 
+                str_size * sizeof(WCHAR)
+            );
+            pRes->resource_array[i].dwFileAttributes = FILE_ATTRIBUTE_DIRECTORY;
+        }
+        
+    } else {
+        // request list of configured storages (available romotes) from rclone's config 
+        // TODO: it is unsafe for now need to be sanitized
+        std::string commandString = UTF16toUTF8(
+                wcharstring((WCHAR*)u"rclone lsjson \"").append(wPath.substr(1)).append((WCHAR*)u"\"").data()
+            );
+        const char* command = commandString.c_str();
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command, "r"), pclose);
+        
+        if (!pipe) return (HANDLE)-1; // popen failed
+
+        // gLogProc(gPluginNumber, MSGTYPE_IMPORTANTERROR, UTF8toUTF16(command).data());
+        
+        // Read the output line by line into the buffer
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+            resultString += buffer.data();
+        }
+
+        // gLogProc(gPluginNumber, MSGTYPE_IMPORTANTERROR, UTF8toUTF16(resultString).data());
+
+        // close popen
+        int status = pclose(pipe.release());
+
+        try
+        {
+            /* code */
+        
+        
+            // return (HANDLE)-1;
+
+            // parse Json string and convert to vector of individual items
+            nlohmann::json resultJson = nlohmann::json::parse(resultString);
+            std::vector<nlohmann::json> resultJsonVector;
+            std::for_each(resultJson.begin(), resultJson.end(), [&resultJsonVector](nlohmann::json &resultLine) {
+                resultJsonVector.push_back(resultLine);
+            });
+
+            pRes = new tResources;
+            pRes->nCount = 0;
+            pRes->resource_array.resize(resultJsonVector.size());
+
+            size_t str_size;
+            for(int i = 0; i < resultJsonVector.size(); i++) 
+            {
+                wcharstring itemName = UTF8toUTF16(resultJsonVector[i]["Name"]);
+                str_size = (MAX_PATH > itemName.size()+1) ? 
+                    (itemName.size()+1): MAX_PATH;
+                memcpy(
+                    pRes->resource_array[i].cFileName, 
+                    itemName.data(), 
+                    str_size * sizeof(WCHAR)
+                );
+                if(resultJsonVector[i]["IsDir"])
+                    pRes->resource_array[i].dwFileAttributes = FILE_ATTRIBUTE_DIRECTORY;
+                if(resultJsonVector[i]["Size"] != -1) {
+                    pRes->resource_array[i].nFileSizeLow = static_cast<DWORD>((uint64_t)resultJsonVector[i]["Size"] & 0xFFFFFFFF);
+                    pRes->resource_array[i].nFileSizeHigh = static_cast<DWORD>((uint64_t)resultJsonVector[i]["Size"] >> 32);
+                }
+                pRes->resource_array[i].ftLastWriteTime = get_file_time(resultJsonVector[i]["ModTime"]);
+
+            }
+
+        }
+        catch(const std::exception& e)
+        {
+            gLogProc(gPluginNumber, MSGTYPE_IMPORTANTERROR, UTF8toUTF16(e.what()).data());
+        }
+    }
+
+    if(!pRes || pRes->resource_array.size()==0)
+        return (HANDLE)-1;
+
+    if(pRes->resource_array.size()>0){
+        memset(FindData, 0, sizeof(WIN32_FIND_DATAW));
+        memcpy(FindData, &(pRes->resource_array[0]), sizeof(WIN32_FIND_DATAW));
+        pRes->nCount++;
+    }
+
+    return (HANDLE) pRes;
 }
 
 BOOL DCPCALL FsFindNextW(HANDLE Hdl, WIN32_FIND_DATAW *FindData)
 {
-    /* not implemented yet */
-    return false;
+    pResources pRes = (pResources) Hdl;
+
+    if(pRes && (pRes->nCount < pRes->resource_array.size()) ){
+        memcpy(FindData, &pRes->resource_array[pRes->nCount], sizeof(WIN32_FIND_DATAW));
+        pRes->nCount++;
+        return true;
+    } else {
+        if(pRes != NULL)
+        {
+            pRes->resource_array.clear();
+            delete pRes;
+        }
+        return false;
+    }
 }
 
 int DCPCALL FsFindClose(HANDLE Hdl) 
 {
-    /* not implemented yet */
+    // this function is not even called
     return 0;
 }
 
---
+++ utils.hpp
@@ -0,0 +1,104 @@
+/*
+    This is modified version of original code copied from (accessed 22 nov 2025):
+    https://github.com/ivanenko/cloud_storage/blob/master/plugin_utils.h
+
+Copyright (C) 2019 Ivanenko Danil (ivanenko.danil@gmail.com)
+Copyright (C) 2026 Iskander Sultanov (BigIskander@gmail.com)
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+        License as published by the Free Software Foundation; either
+        version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+        Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+        Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
+#ifndef _UTILS_HPP
+#define _UTILS_HPP
+
+#include "common.h"
+#include <codecvt>
+#include <locale>
+#include <vector>
+#include <sys/stat.h>
+#include <iomanip>
+
+typedef struct {
+    int nCount;
+    std::vector<WIN32_FIND_DATAW> resource_array;
+} tResources, *pResources;
+
+typedef std::basic_string<WCHAR> wcharstring;
+
+FILETIME get_now_time()
+{
+    time_t t2 = time(0);
+    int64_t ft = (int64_t) t2 * 10000000 + 116444736000000000;
+    FILETIME file_time;
+    // deleted: &0xffff - it causes time shift to ~5 min
+    file_time.dwLowDateTime = ft;
+    file_time.dwHighDateTime = ft >> 32;
+    return file_time;
+}
+
+// add function converting RFC3339 standard time sting to FILETIME
+FILETIME get_file_time(std::string tm)
+{
+    // convert std::string to time_t
+    std::tm t;
+    std::istringstream ss(tm);
+    ss >> std::get_time(&t, "%Y-%m-%dT%H:%M:%S");
+    time_t ttime = std::mktime(const_cast<std::tm*>(&t));
+    // convert time_t to FILETIME
+    int64_t ft = (int64_t) ttime * 10000000 + 116444736000000000;
+    FILETIME file_time;
+    // 
+    file_time.dwLowDateTime = ft;
+    file_time.dwHighDateTime = ft >> 32;
+    return file_time;
+}
+
+std::string UTF16toUTF8(const WCHAR *p)
+{
+    // replaced codecvt_utf8 with codecvt_utf8_utf16 to fix an conversion error
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert2;
+    return convert2.to_bytes(std::u16string((char16_t*) p));
+}
+
+wcharstring UTF8toUTF16(const std::string &str)
+{
+    // replaced codecvt_utf8 with codecvt_utf8_utf16 to fix an conversion error
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert2;
+    std::u16string utf16 = convert2.from_bytes(str);
+    return wcharstring((WCHAR*)utf16.data());
+}
+
+BOOL file_exists(const std::string& filename)
+{
+    struct stat buf;
+    return stat(filename.c_str(), &buf) == 0;
+}
+
+// add function to gel local file size
+BOOL get_file_size(const std::string& filename, uint64_t& fileSize)
+{
+    struct stat buf;
+    if(stat(filename.c_str(), &buf) != 0) return false;
+    fileSize = buf.st_size;
+    return true;
+}
+
+// add function to convert int to wcharstring
+wcharstring int_to_wcharstring(int num) 
+{
+    return UTF8toUTF16(std::to_string(num));
+}
+
+#endif
